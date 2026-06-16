ba1c10a45ec30fbd28e979d1f71170b11fcd9726
josue <josue2014cs@gmail.com>
2026-06-16 00:48:31 -0300

    feat(testbench): testes realizados e aprovados dos modulos ula/regfile/pc

---
+++ tests/tb_ula.v
@@ -0,0 +1,106 @@
+module tb_ula;
+
+    reg [31:0] in1;
+    reg [31:0] in2;
+    reg [3:0] op;
+
+    wire [31:0] result;
+    wire zeroFlag;
+
+    // Instancia a ULA
+    ula uut (
+
+        .in1(in1),
+        .in2(in2),
+        .op(op),
+
+        .result(result),
+        .zeroFlag(zeroFlag)
+
+    );
+
+    initial begin
+
+        // 10 + 20 = 30
+        in1 = 32'd10;
+        in2 = 32'd20;
+        op  = 4'b0000;
+
+        #10;
+
+        // 20 - 10 = 10
+        in1 = 32'd20;
+        in2 = 32'd10;
+        op  = 4'b0001;
+
+        #10;
+
+        // 15 - 15 = zeroFlag
+        in1 = 32'd15;
+        in2 = 32'd15;
+        op  = 4'b0001;
+
+        #10;
+
+        // 12 & 10 = 8
+        in1 = 32'd12;
+        in2 = 32'd10;
+        op  = 4'b0010;
+
+        #10;
+
+        // 12 | 10 = 14
+        op = 4'b0011;
+
+        #10;
+
+        // 12 ^ 10 = 6
+        op = 4'b0100;
+
+        #10;
+
+        // ~(12 | 10) = 1 [NOR]
+        op = 4'b0101;
+
+        #10;
+
+        // -5 < 3 (com sinal)
+        in1 = -32'd5;
+        in2 = 32'd3;
+        op  = 4'b0110;
+
+        #10;
+		  
+		  // 5 < 3 (sem sinal)
+        in1 = 32'd5;
+        in2 = 32'd10;
+        op  = 4'b0111;
+
+        #10;
+
+        // 3 << 2 = 12 (sll)
+        in1 = 32'd2;
+        in2 = 32'd3;
+        op  = 4'b1000;
+
+        #10;
+
+        // 16 >> 2 = 4 (srl)
+        in1 = 32'd2;
+        in2 = 32'd16;
+        op  = 4'b1001;
+
+        #10;
+
+        // -16 >>> 2 = -4 (sra)
+        in1 = 32'd2;
+        in2 = -32'd16;
+        op  = 4'b1010;
+
+        #10;
+
+        $stop;
+
+    end
+
+endmodule
