f738c931f1e60c51833deeacad04a9b5b4a2d988
JosueC006 <josue2014cs@gmail.com>
2026-06-18 23:30:32 -0300

    Update adder.v

--- hardware/adder.v
+++ hardware/adder.v
@@ -1,3 +1,10 @@
+// ============================================================
+// Grupo: Marcos José e Josué Costa
+// Descrição: Somador de 32 bits utilizado para realizar
+//            operações de incremento e cálculo de endereços,
+//            como PC+4, PC+8 e endereços de branch.
+// ============================================================
+
 module adder(
 
     input [31:0] a,
