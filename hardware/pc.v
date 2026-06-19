fc804c6cd18b0fd018bf0e05ce86055373042aa5
JosueC006 <josue2014cs@gmail.com>
2026-06-18 23:01:19 -0300

    Update pc.v

--- hardware/pc.v
+++ hardware/pc.v
@@ -1,24 +1,5 @@
-<<<<<<< HEAD
-module pc (
-    input clock,
-    input [31:0] nextPC,
-    output reg [31:0] PC
-);
-
-// Garante que o PC começa em zero
-initial begin
-    PC = 32'h00000000;
-end
-
-// Executa somente na borda de subida do clock
-always @(posedge clock) begin
-    PC <= nextPC;
-end
-
-endmodule
-=======
 // ============================================================
-// Grupo: --
+// Grupo: Marcos José e Josué Costa
 // Atividade: Projeto 02 - 2VA
 // Disciplina: Arquitetura e Organização de Computadores
 // Semestre: 2025.2
@@ -49,4 +30,3 @@ module pc (
     end
 
 endmodule
->>>>>>> feat/complete-mips-implementation
