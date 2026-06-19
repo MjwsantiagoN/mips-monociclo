ec9aa7b8dc80f60491ba106b3cb622ee8dd42256
JosueC006 <josue2014cs@gmail.com>
2026-06-18 23:37:44 -0300

    Update comparator.v

--- hardware/comparator.v
+++ hardware/comparator.v
@@ -1,3 +1,10 @@
+// ============================================================
+// Grupo: Marcos José e Josué Costa
+// Descrição: Comparador utilizado para verificar a relação
+//            entre dois operandos, auxiliando em operações
+//            de comparação e instruções condicionais.
+// ============================================================
+
 module comparator(
 
     input [31:0] a,
