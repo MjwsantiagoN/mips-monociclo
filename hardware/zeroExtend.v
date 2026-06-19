96b2174b1d201969b349067b9bc2923e213877a9
JosueC006 <josue2014cs@gmail.com>
2026-06-18 23:37:01 -0300

    Update zeroExtend.v

--- hardware/zeroExtend.v
+++ hardware/zeroExtend.v
@@ -1,3 +1,10 @@
+// ============================================================
+// Grupo: Marcos José e Josué Costa
+// Descrição: Módulo de extensão com zero.
+//            Converte um valor de 16 bits para 32 bits,
+//            preenchendo os bits mais significativos com zero.
+// ============================================================
+
 module zeroExtend (
 
     input  [15:0] in,
