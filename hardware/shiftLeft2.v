d9637528235b570fd8ac21ea54c1d7588a6de9eb
JosueC006 <josue2014cs@gmail.com>
2026-06-18 23:36:40 -0300

    Update shiftLeft2.v

--- hardware/shiftLeft2.v
+++ hardware/shiftLeft2.v
@@ -1,3 +1,10 @@
+// ============================================================
+// Grupo: Marcos José e Josué Costa
+// Descrição: Módulo responsável por deslocar um valor
+//            2 bits para a esquerda.
+//            Utilizado no cálculo dos endereços de branch.
+// ============================================================
+
 module shiftLeft2 (
 
     input  [31:0] in,
